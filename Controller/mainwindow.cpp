55b76367e1b0492f66cda877ccbe87ec0774c312
Juan VF <juanvfletes@estudiantec.cr>
2020-12-07 13:41:13 -0600

    Initial Design

---
+++ Controller/mainwindow.cpp
@@ -0,0 +1,11 @@
+#include <Controller/mainwindow.h>
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow){
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow(){
+    delete ui;
+}
+
